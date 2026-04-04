be1dd0256cc49aa3b465fb286c16cb853c4c229e
masterShashi <shashikumargupta443@gmail.com>
2026-04-04 17:37:50 +0530

    Reverse Proxy to handle Preview URLs added

--- lovable/src/main/java/com/devshashi/lovable/service/impl/KubernetesDeploymentServiceImpl.java
+++ lovable/src/main/java/com/devshashi/lovable/service/impl/KubernetesDeploymentServiceImpl.java
@@ -8,6 +8,7 @@ import io.fabric8.kubernetes.client.dsl.ExecListener;
 import io.fabric8.kubernetes.client.dsl.ExecWatch;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.io.ByteArrayOutputStream;
@@ -20,6 +21,7 @@ import java.util.concurrent.TimeUnit;
 public class KubernetesDeploymentServiceImpl implements DeploymentService {
 
     private final KubernetesClient client;
+    private final StringRedisTemplate redisTemplate;
 
     private static final String NAMESPACE = "lovable-app";
     private static final String POOL_LABEL = "status";
@@ -38,6 +40,7 @@ public class KubernetesDeploymentServiceImpl implements DeploymentService {
         Pod existingPod = findActivePod(projectId);
 
         if(existingPod != null){
+            registerRoute(domain, existingPod);
             return new DeployResponse("http://" + domain + ":" + REVERSE_PROXY_PORT);
         }
 
@@ -84,6 +87,8 @@ public class KubernetesDeploymentServiceImpl implements DeploymentService {
             log.info("Starting dev server for project {}...", projectId);
             execCommand(podName, RUNNER_CONTAINER, "sh", "-c", startCmd);
 
+            registerRoute(domain, pod);
+
             log.info("Deployment Successful: http://{}:{}", domain, REVERSE_PROXY_PORT);
             return new DeployResponse("http://" + domain + ":" + REVERSE_PROXY_PORT);
         } catch(Exception e){
@@ -91,6 +96,13 @@ public class KubernetesDeploymentServiceImpl implements DeploymentService {
             client.pods().inNamespace(NAMESPACE).withName(podName).delete();
             throw new RuntimeException("Failed to deploy the project with id: " + projectId);
         }
+    }
+
+    private void registerRoute(String domain, Pod pod){
+        String podIp = pod.getStatus().getPodIP();
+        if(podIp == null) throw new RuntimeException("Pod is running but has no IP!");
+
+        redisTemplate.opsForValue().set("route:" + domain, podIp + ":5173", 6, TimeUnit.HOURS);
     }
 
     private void execCommand(String podName, String container, String... command){
